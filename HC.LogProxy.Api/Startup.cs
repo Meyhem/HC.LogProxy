a4e43e22de6bee8cc4313a2ca015d4220ea39316
MM <m.meriac@consultant.crif.com>
2021-10-28 09:41:03 +0200

    Ports Docker Dev settings Readme

--- HC.LogProxy.Api/Startup.cs
+++ HC.LogProxy.Api/Startup.cs
@@ -52,14 +52,9 @@ namespace HC.LogProxy.Api
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HC.LogProxy.Api v1"));
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HC.LogProxy.Api v1"));
 
-            app.UseHttpsRedirection();
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
---
+++ Readme.md
@@ -0,0 +1,15 @@
+# HC.LogProxy
+
+## Running in Docker
+```sh
+docker pull meyhem/hclogproxy:latest
+docker run --name "HCLogProxy" -p 80:80 meyhem/hclogproxy:latest
+```
+
+## Running from CLI
+```sh
+cd <project root>/HC.LogProxy.Api
+dotnet publish --configuration Release --output HcLogProxyPackage
+cd HcLogProxyPackage
+dotnet HC.LogProxy.Api.dll
+```
